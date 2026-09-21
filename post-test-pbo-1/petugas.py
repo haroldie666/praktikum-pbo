871052b8a95785a6968e059e26cbe56a92f7f430
senku_maniac <devanoargya6@gmail.com>
2026-09-22 01:13:37 +0800

    add: class petugas

---
+++ post-test-pbo-1/petugas.py
@@ -0,0 +1,30 @@
+class Petugas:
+    # Atribut Kelas
+    total_petugas = 0
+
+    def __init__(self, nama, id_petugas):
+        self.nama = nama
+        self.__id_petugas = None
+        self.id_petugas = id_petugas
+        
+        Petugas.total_petugas += 1
+
+    @property
+    def id_petugas(self):
+        return self.__id_petugas
+
+    @id_petugas.setter
+    def id_petugas(self, id_baru):
+        if not Petugas.validasi_format_id(id_baru):
+            raise ValueError("Format ID Petugas tidak valid! Harus diawali dengan 'PTG'.")
+        self.__id_petugas = id_baru
+
+    @staticmethod
+    def validasi_format_id(id_teks):
+        return str(id_teks).startswith("PTG")
+
+    def layani_donor(self, pendonor, bank_darah):
+        print(f"\nPetugas {self.nama} sedang memproses donor atas nama {pendonor.nama}...")
+        if pendonor.cek_kelayakan():
+            print(f"Proses donor darah {pendonor.golongan_darah} berhasil dilakukan.")
+            bank_darah.stok_darah = bank_darah.stok_darah + 1
